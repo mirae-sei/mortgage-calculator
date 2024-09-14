35ee9b06bd39fd1fc7aa0117f9f2b4a361c9e792
mirae-sei <blessdugan@gmail.com>
2024-09-14 15:49:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -19,16 +19,39 @@ const interestRate = parseFloat(document.querySelector('.interestRate').value) /
 const mortgageType = document.querySelector('input[name="mortgageType"]:checked');
 
 
+
+let inputMortgageAmount = document.querySelector('.inputMortgageAmount');
+let inputMortgageTerm = document.querySelector('.mortgageTermInput');
+let inputInterestRate = document.querySelector('.interestRateInput');
+
+
+
+let mortgageAmountError = document.querySelector('.mortgage-amount-error');
+let mortgageTermError = document.querySelector('.mortgage-term-error');
+let interestRateError = document.querySelector('.interest-rate-error');
+let radioError = document.querySelector('.radio-error');
+
+
+
+
 let monthlyPayment = 0 ;
 let totalRepayment = 0 ;
+let isValid = true ;
 
 if (isNaN(mortgageAmount) || isNaN(mortgageTerm) || isNaN(interestRate) ||
-    mortgageAmount < 0 || mortgageTerm < 0 || interestRate < 0) {
-     console.log('Please enter a valid number');
-     console.log(mortgageType.value)
+    mortgageAmount < 0 || mortgageTerm < 0 || interestRate < 0 || mortgageType === null) {
+    isValid = false ;
+    document.querySelector('.default-result').style.display = 'flex';
+    document.querySelector('.result').style.display = 'none';
+     
       
      
 }else {
+  isValid = true;
+  document.querySelector('.default-result').style.display = 'none';
+  document.querySelector('.result').style.display = 'block';
+     
+ 
   if(mortgageType.value === 'repayment'){
     const monthlyRate = interestRate / 12;
     const n = mortgageTerm * 12 ;
@@ -52,8 +75,77 @@ if (isNaN(mortgageAmount) || isNaN(mortgageTerm) || isNaN(interestRate) ||
   
 }
 
+
+if(isNaN(mortgageAmount) || mortgageAmount < 0){
+  mortgageAmountError.style.display = 'block';
+  inputMortgageAmount.style.marginBottom = '5px';
+  inputMortgageAmount.classList.add('border-error-style-1');
+  document.querySelector('.border-error-style-1 p').style.backgroundColor = 'red';
+  document.querySelector('.border-error-style-1 p').style.color = 'white';
   
 
+}else{
+  mortgageAmountError.style.display = 'none';
+  inputMortgageAmount.style.marginBottom = '15px';
+  inputMortgageAmount.classList.remove('border-error-style-1');
+  document.querySelector('.inputMortgageAmount p').style.backgroundColor = 'hsl(202, 86%, 94%)';
+  document.querySelector('.inputMortgageAmount p').style.color = 'hsl(200, 24%, 40%)';
+  
+}
+
+
+
+if(isNaN(mortgageTerm) || mortgageTerm < 0){
+  mortgageTermError.style.display = 'block';
+  inputMortgageTerm.classList.add('border-error-style-2');
+  document.querySelector('.mortgageTermInput p').style.backgroundColor = 'red';
+  document.querySelector('.mortgageTermInput p').style.color = 'white';
+  
+  
+}else{
+  mortgageTermError.style.display = 'none';
+  inputMortgageTerm.classList.remove('border-error-style-2');
+  document.querySelector('.mortgageTermInput p').style.backgroundColor = 'hsl(202, 86%, 94%)';
+  document.querySelector('.mortgageTermInput p').style.color = 'hsl(200, 24%, 40%)';
+  
+
+}
+
+
+
+
+if(isNaN(interestRate) || interestRate < 0){
+  interestRateError.style.display = 'block';
+  inputInterestRate.classList.add('border-error-style-3');
+  document.querySelector('.border-error-style-3 p').style.backgroundColor = 'red';
+  document.querySelector('.border-error-style-3 p').style.color = 'white';
+  
+  
+  
+}else{
+  interestRateError.style.display = 'none';
+  inputInterestRate.classList.remove('border-error-style-3');
+  document.querySelector('.interestRateInput p').style.backgroundColor = 'hsl(202, 86%, 94%)';
+  document.querySelector('.interestRateInput p').style.color = 'hsl(200, 24%, 40%)';
+  
+
+}
+
+
+if(!mortgageType){
+  radioError.style.display = 'block';
+  document.querySelectorAll('.options').forEach(options => {
+    // Apply the color style to each element
+    options.style.color = 'red';
+  });
+}else{
+  radioError.style.display = 'none';
+  document.querySelectorAll('.options').forEach(options => {
+    // Apply the color style to each element
+    options.style.color = 'hsl(202, 55%, 16%)';
+  });
+  
+}
 
 
 
@@ -61,12 +153,70 @@ if (isNaN(mortgageAmount) || isNaN(mortgageTerm) || isNaN(interestRate) ||
 
 
 
-
-
+  /*if (isChecked) {
+    
+    radioError.style.display = 'none';
+  
+   
+  } else {
+    
+    radioError.style.display = 'block';
+  }*/
 
 
 
 })
+
+/*document.querySelector('.clear-all').addEventListener('click' ,  function(){
+  const mortgageAmount = parseFloat(document.querySelector('.mortgageAmount').reset);
+const mortgageTerm = parseFloat(document.querySelector('.mortgageTerm').reset);
+const interestRate = parseFloat(document.querySelector('.interestRate').value).reset ;
+const mortgageType = document.querySelector('input[name="mortgageType"]:checked').reset;
+
+mortgageAmount = 0 ;
+mortgageTerm.reset();
+mortgageType.reset();
+interestRate.reset();
+
+  
+})*/
+
+
+document.querySelector('.clear-all').addEventListener('click', function() {
+  // Select the form element
+  let inputForm = document.querySelector('form');
+  document.querySelector('.default-result').style.display = 'flex';
+  document.querySelector('.result').style.display = 'none';
+
+  inputForm.reset();
+
+
+});
+
+
+
+
+
+//for active state
+
+document.querySelectorAll('.mortgage-type').forEach(input =>{
+  input.addEventListener('change' , () => {
+    document.querySelectorAll('.options').forEach(div => {
+      div.classList.remove('selected');
+    })
+
+    if(document.querySelector('.mortgage-type').checked){
+      div.classList.add('selected');
+    }
+  })
+})
+
+
+
+
+
+
+
 
 
 
